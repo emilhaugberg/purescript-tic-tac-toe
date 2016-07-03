c31f57d3237f24cc5b93f0f26f8d95355a255f63
Emil <emil.haugberg@tinker.travel>
2016-07-03 23:40:30 +0200

    Hacky way to remove bug that updates player even when block is already clicked.

--- src/Main.purs
+++ src/Main.purs
@@ -1,6 +1,6 @@
 module Main where
 
-import Prelude (class Show, Unit, bind, show, const, map, (<<<), (==), (<>))
+import Prelude (class Show, Unit, bind, show, const, map, (<<<), (==), (<>), (-))
 
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Exception (EXCEPTION)
@@ -12,7 +12,8 @@ import Pux.Html.Events (onClick)
 import Pux.Html.Attributes (className)
 
 import Data.Tuple (Tuple(..), snd, fst)
-import Data.Array (range)
+import Data.Array (range, index)
+import Data.Maybe
 
 data Action = Click Int
             | Reset
@@ -26,9 +27,21 @@ type State = { activePlayer :: Token, board :: Board }
 initState :: State
 initState = { activePlayer: X, board: map (\i -> Tuple i E) (range 1 9) }
 
+-- update :: Action -> State -> State
+-- update (Click i) st = { activePlayer: updatePlayer st.activePlayer, board: updateBoard i st }
+-- update Reset st = initState
+
 update :: Action -> State -> State
-update (Click i) st = { activePlayer: updatePlayer st.activePlayer, board: updateBoard i st }
+update (Click i) st = updateState i st
 update Reset st = initState
+
+updateState :: Position -> State  -> State
+updateState pos st =
+  case index st.board (pos - 1) of
+    Just (Tuple pos X) -> st
+    Just (Tuple pos O) -> st
+    Just (Tuple pos E) -> { activePlayer: updatePlayer st.activePlayer, board: updateBoard pos st }
+    Nothing -> st
 
 updatePlayer :: Token -> Token
 updatePlayer tk =
@@ -37,14 +50,17 @@ updatePlayer tk =
     O -> X
     E -> E
 
-updateBoard :: Int -> State -> Board
+updateBoard :: Position -> State -> Board
 updateBoard pos st =
   map f st.board
   where
+    f :: Tuple Position Token -> Tuple Position Token
     f tup = if fst tup == pos
-               then g tup
+               then checkIfAlreadyClicked tup
                else tup
-    g tup =
+
+    checkIfAlreadyClicked :: Tuple Position Token -> Tuple Position Token
+    checkIfAlreadyClicked tup =
       case snd tup of
         X -> tup
         O -> tup
@@ -67,7 +83,7 @@ view gmState =
     blocks = map mkBlock gmState.board
 
     mkBlock :: Tuple Int Token -> Html Action
-    mkBlock tup = div [className ("block " <> (show <<< snd) tup), onClick (const (Click (fst tup)))] [(text <<< show) (snd tup)]
+    mkBlock tup = div [className ("block " <> (show <<< snd) tup <> " " <> (show <<< fst) tup), onClick (const (Click (fst tup)))] [(text <<< show) (snd tup)]
 
 main :: forall e. Eff (err :: EXCEPTION, channel :: CHANNEL | e) Unit
 main = do
